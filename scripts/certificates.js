07eb1325877217a8b820c14f142e884c909a98ba
Youssef <Youssef.Mo7amed@gmail.com>
2024-09-26 14:59:19 +0300

    certificates: Adding AWS Academy Cloud Architecting

--- scripts/certificates.js
+++ scripts/certificates.js
@@ -1,29 +1,37 @@
 // Data: List of certificates
 let certificatesData = [
-    {
-        name: "AWS Academy Graduate - AWS Academy Cloud Foundations",
-        issuer: "Amazon Web Services Training and Certification",
-        dates: "August 2024",
-        description:
-            "An introduction to cloud computing, security, architecture, pricing, and support, with hands-on experience using AWS services.",
-        link: "https://www.credly.com/badges/9eb0a106-d672-4dba-b64e-06fbf17f03d4",
-    },
-    {
-        name: "Data Engineering with AWS",
-        issuer: "Udacity",
-        dates: "April 2023",
-        description:
-            "This Nanodegree program by Udacity covers the concepts and skills needed to be successful as a data engineer, including designing and implementing data pipelines, architecting data warehouses, and working with data processing frameworks like Apache Spark.",
-        link: "https://www.udacity.com/certificate/3SWGNAF7",
-    },
-    {
-        name: "Google Cloud Big Data and Machine Learning Fundamentals",
-        issuer: "Coursera by Google Cloud Training",
-        dates: "April 2022",
-        description:
-            "This certificate is awarded by Google Cloud Training for completing the Google Cloud Big Data and Machine Learning Fundamentals course on Coursera. The course covers the fundamentals of big data and machine learning on Google Cloud Platform, including data processing, storage, and querying, as well as the use of machine learning APIs and AutoML.",
-        link: "https://coursera.org/share/7823fac35479966f10bc25e592ffa259",
-    },
+	{
+		name: "AWS Academy Graduate - AWS Academy Cloud Architecting",
+		issuer: "Amazon Web Services Training and Certification",
+		dates: "September 2024",
+		description:
+			"A comprehensive overview of cloud computing, security, architecture, pricing, and support, with hands-on experience using AWS services.",
+		link: "https://www.credly.com/badges/06d2d8fc-06a1-4a73-8aad-c39f2d2659f1",
+	},
+	{
+		name: "AWS Academy Graduate - AWS Academy Cloud Foundations",
+		issuer: "Amazon Web Services Training and Certification",
+		dates: "August 2024",
+		description:
+			"An introduction to cloud computing, security, architecture, pricing, and support, with hands-on experience using AWS services.",
+		link: "https://www.credly.com/badges/9eb0a106-d672-4dba-b64e-06fbf17f03d4",
+	},
+	{
+		name: "Data Engineering with AWS",
+		issuer: "Udacity",
+		dates: "April 2023",
+		description:
+			"This Nanodegree program by Udacity covers the concepts and skills needed to be successful as a data engineer, including designing and implementing data pipelines, architecting data warehouses, and working with data processing frameworks like Apache Spark.",
+		link: "https://www.udacity.com/certificate/3SWGNAF7",
+	},
+	{
+		name: "Google Cloud Big Data and Machine Learning Fundamentals",
+		issuer: "Coursera by Google Cloud Training",
+		dates: "April 2022",
+		description:
+			"This certificate is awarded by Google Cloud Training for completing the Google Cloud Big Data and Machine Learning Fundamentals course on Coursera. The course covers the fundamentals of big data and machine learning on Google Cloud Platform, including data processing, storage, and querying, as well as the use of machine learning APIs and AutoML.",
+		link: "https://coursera.org/share/7823fac35479966f10bc25e592ffa259",
+	},
 ];
 
 // Function to render certificates
